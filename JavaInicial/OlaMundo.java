78785c771c75d647fc66b0bf8f9d28f14975c953
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-11-16 13:26:26 -0300

    add primeiro codigo em java

---
+++ JavaInicial/OlaMundo.java
@@ -0,0 +1,14 @@
+package JavaInicial;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		
+		String nome = "Grazi";
+		int idade = 20;
+		double altura = 1.54;
+		
+		System.out.println("\n nome: " + nome + " idade: " + idade + " altura: " + altura);
+	}
+
+}

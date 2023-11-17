281a03fd3b6c7782bf9d9cec9296603f712fd79b
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-11-17 13:25:45 -0300

    add classe Calculadora

---
+++ poo/Calculadora.java
@@ -0,0 +1,24 @@
+package poo;
+
+public class Calculadora {
+	public static float somar(float n1, float n2) {
+		return n1+n2;
+	}
+	
+	public static float subtrair(float n1,float n2) {
+		return n1-n2;
+	}
+	
+	public static float multiplicar(float n1,float n2) {
+		return n1*n2;
+	}
+	
+	public static float dividir(float n1,float n2) {
+		return n1/n2;
+	}
+	
+	public static void frase() {
+		System.out.println("\nEstamos aprendendo mÃ©todos em Java!!!");
+	}
+
+}

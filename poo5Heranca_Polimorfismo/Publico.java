e7dcbd6a4b9aca75e01b650ef5c6848c60e41a28
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-11-21 23:53:12 -0300

    add classe Publico

---
+++ poo5Heranca_Polimorfismo/Publico.java
@@ -0,0 +1,27 @@
+package poo5Heranca_Polimorfismo;
+
+public class Publico extends Telefone{
+	
+	public Publico() {
+		super("Telefone Público");
+	}
+	
+	@Override
+	public void toca(int numToques) {
+		for(int i=0;i<numToques;i++) {
+			System.out.println("\nDingdin Dingdin");
+		}
+	}
+	
+	@Override
+	public void disca(String numero) {
+		if(numero.charAt(0)=='9' || numero.charAt(0)=='8') {
+			System.out.println("\nEste telefone não liga para celular...");
+		}else {
+			System.out.println("\nDiscando: "+numero);
+		}
+	}
+
+}
+
+

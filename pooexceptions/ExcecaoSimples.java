328203732e0138487839aefde00ae6bfc21f6442
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-11-23 22:32:22 -0300

    add class ExcecaoSimples

---
+++ pooexceptions/ExcecaoSimples.java
@@ -0,0 +1,12 @@
+package pooexceptions;
+
+public class ExcecaoSimples extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ExcecaoSimples() {}
+	
+	public ExcecaoSimples(String message) {
+		super(message);
+	}
+
+}

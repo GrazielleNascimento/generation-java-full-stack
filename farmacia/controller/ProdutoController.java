e5805e97a00233d997963780aa847e5056fe590d
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-11-26 15:54:12 -0300

    implementa interface ProdutoRepository

--- farmacia/controller/ProdutoController.java
+++ farmacia/controller/ProdutoController.java
@@ -1,5 +1,37 @@
 package farmacia.controller;
 
-public class ProdutoController {
+import farmacia.interfaces.ProdutoRepository;
+
+public class ProdutoController implements ProdutoRepository{
+
+	@Override
+	public void criarProduto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listarProdutos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void consultarPorId() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizarProduto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletarProduto() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }

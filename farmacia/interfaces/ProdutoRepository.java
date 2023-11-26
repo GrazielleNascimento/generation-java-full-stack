331f2e5c9313dc478d3868c96eea3eb2b1e364cc
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-11-26 15:49:26 -0300

    add interface ProdutoRepository

---
+++ farmacia/interfaces/ProdutoRepository.java
@@ -0,0 +1,15 @@
+package farmacia.interfaces;
+
+public interface ProdutoRepository {
+	
+	public void criarProduto();
+	
+	public void listarProdutos();
+	
+	public void consultarPorId();
+	
+	public void atualizarProduto();
+	
+	public void deletarProduto();
+	
+}

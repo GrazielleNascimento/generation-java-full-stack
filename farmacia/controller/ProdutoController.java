ba2fb1a0d4bb49b2c3a4074ff2c302d21f585b0a
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-11-26 17:35:01 -0300

    add impletacao dos metodos  CRUD da controller

--- farmacia/controller/ProdutoController.java
+++ farmacia/controller/ProdutoController.java
@@ -1,37 +1,65 @@
+
 package farmacia.controller;
 
-import farmacia.interfaces.ProdutoRepository;
+import java.util.ArrayList;
+import java.util.Optional;
 
-public class ProdutoController implements ProdutoRepository{
+import farmacia.interfaces.ProdutoRepository;
+import farmacia.model.Produto;
+
+public class ProdutoController implements ProdutoRepository {
+
+	// criar a Collection
+	private ArrayList<Produto> listaProdutos = new ArrayList<>();
 
 	@Override
-	public void criarProduto() {
-		// TODO Auto-generated method stub
-		
+	public void criarProduto(Produto produto) {
+		listaProdutos.add(produto);
+		System.out.println("Produto: " + produto.getId() + " foi criado com sucesso");
 	}
 
 	@Override
 	public void listarProdutos() {
-		// TODO Auto-generated method stub
-		
+		for (Produto produto : listaProdutos) {
+			produto.visualizar();
+		}
 	}
 
 	@Override
-	public void consultarPorId() {
-		// TODO Auto-generated method stub
-		
+	public void consultarPorId(int id) {
+		Optional<Produto> opProduto = buscarNaCollection(id);
+		if (opProduto.isPresent()) {
+			System.out.println("PRODUTO: " + opProduto.get());// opProduto.get() retorna um objeto do optional
+		} else {
+			System.out.println("O Produto do id:  " + id + " não foi cadastrado");
+		}
+
 	}
 
 	@Override
-	public void atualizarProduto() {
-		// TODO Auto-generated method stub
-		
+	public void atualizarProduto(Produto produto) {
+		Optional<Produto> opProduto = buscarNaCollection(produto.getId());
+		if (opProduto.isPresent()) {
+			listaProdutos.set(listaProdutos.indexOf(opProduto.get()), produto);
+			System.out.println("Produto do id: " + produto.getId() + " foi atualizado com sucesso!");
+		} else {
+			System.out.println("Produto do id " + produto.getId() + " não encontrado!");
+		}
 	}
 
 	@Override
-	public void deletarProduto() {
-		// TODO Auto-generated method stub
-		
+	public void deletarProduto(int id) {
+		Optional<Produto> opProduto = buscarNaCollection(id);
+		if(opProduto.isPresent()) {
+			if(listaProdutos.remove(opProduto.get()) == true) {
+				System.out.println("Produto deletado");
+			}
+			else {
+				System.out.println("Produto não encontrado");
+			}
+		}
+
 	}
+	
 
 }

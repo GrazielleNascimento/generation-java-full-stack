6f2e0d66ed5ce7cdc5bd5b7ee3d56c47d3948085
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-11-26 16:14:46 -0300

    add parametros dos metodos

--- farmacia/interfaces/ProdutoRepository.java
+++ farmacia/interfaces/ProdutoRepository.java
@@ -1,15 +1,17 @@
 package farmacia.interfaces;
 
+import farmacia.model.Produto;
+
 public interface ProdutoRepository {
 	
-	public void criarProduto();
+	public void criarProduto(Produto produto);
 	
 	public void listarProdutos();
 	
-	public void consultarPorId();
+	public void consultarPorId(int id);
 	
-	public void atualizarProduto();
+	public void atualizarProduto(Produto produto);
 	
-	public void deletarProduto();
+	public void deletarProduto(int id);
 	
 }
